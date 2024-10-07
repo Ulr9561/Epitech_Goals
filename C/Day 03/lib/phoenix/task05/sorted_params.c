5faf6643d6aaa0f2f7f4278c767f857a139e810e
Ulrich ADEGOKE <adegokeulrich1@gmail.com>
2024-10-07 18:19:07 +0100

    day03: finished

--- C/Day 03/lib/phoenix/task05/sorted_params.c
+++ C/Day 03/lib/phoenix/task05/sorted_params.c
@@ -2,6 +2,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-    
+int compare_strings(const void *a, const void *b)
+{
+    const char **str_1 = (const char **)a;
+    const char **str_2 = (const char **)b;
+
+    return strcmp(*str_1, *str_2);
+}
+
+int main(int argc, char *argv[])
+{
+    qsort(argv, argc, sizeof(char *), compare_strings);
+
+    for (int i = 0; i < argc; i++)
+    {
+        printf("%s\n", argv[i]);
+    }
+
+    return 0;
 }
